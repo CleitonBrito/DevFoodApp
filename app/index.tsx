18de2fdde0ec7a6a135dedaae8f52ffb59e6fd8a
CleitonBrito <cleytonbritto3003@gmail.com>
2024-07-15 14:21:32 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-slate-800">
+      <Text className="text-slate-50 text-xl font-light">Hello, my name is Cleiton Brito</Text>
+      <Text className="text-green-400 text-sm font-thin">It's my first app in Expo.</Text>
+    </View>
+  );
+}
