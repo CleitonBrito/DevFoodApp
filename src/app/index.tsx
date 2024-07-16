780c89edfc46ef1e1c4e4a7668ed48cacd9b762d
CleitonBrito <cleytonbritto3003@gmail.com>
2024-07-15 23:33:50 -0300

    create header component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,19 @@
-import { Text, View } from "react-native";
+import { Header } from "@/components/header";
+import { Text, View, ScrollView } from "react-native";
+
+import Constants from "expo-constants";
+const statusBarHeight = Constants.statusBarHeight
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-slate-800">
-      <Text className="text-slate-50 text-xl font-light">Hello, my name is Cleiton Brito</Text>
-      <Text className="text-green-400 text-sm font-thin">It's my first app in Expo.</Text>
-    </View>
+    <ScrollView 
+        style={{ flex: 1 }} 
+        className="bg-slate-200"
+        showsVerticalScrollIndicator={false}
+    >
+        <View className="w-full px-4" style={{marginTop: statusBarHeight + 8}}>
+            <Header />
+        </View>
+    </ScrollView>
   );
 }
