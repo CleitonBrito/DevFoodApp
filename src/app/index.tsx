355e798aaa564f5cbed64ba32a680be066987894
CleitonBrito <cleytonbritto3003@gmail.com>
2024-07-17 23:02:59 -0300

    component search

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
-import { Header } from "@/components/header";
 import { Text, View, ScrollView } from "react-native";
-
 import Constants from "expo-constants";
+
+import { Header } from "@/components/header";
+import { Search } from "@/components/Search";
+
 const statusBarHeight = Constants.statusBarHeight
 
 export default function Index() {
@@ -13,6 +15,8 @@ export default function Index() {
     >
         <View className="w-full px-4" style={{marginTop: statusBarHeight + 8}}>
             <Header />
+
+            <Search />
         </View>
     </ScrollView>
   );
