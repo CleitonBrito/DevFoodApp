dd89dc6ee6a41e3fe3899a4aab5d6aa22ebceb69
CleitonBrito <cleytonbritto3003@gmail.com>
2024-07-17 23:22:24 -0300

    component section

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,8 @@ import { Text, View, ScrollView } from "react-native";
 import Constants from "expo-constants";
 
 import { Header } from "@/components/header";
-import { Search } from "@/components/Search";
+import { Search } from "@/components/search";
+import { Section } from "@/components/section";
 
 const statusBarHeight = Constants.statusBarHeight
 
@@ -15,8 +16,13 @@ export default function Index() {
     >
         <View className="w-full px-4" style={{marginTop: statusBarHeight + 8}}>
             <Header />
-
             <Search />
+            <Section 
+                name="Comidas em alta"
+                label="Veja mais"
+                size="text-2xl"
+                action={ () => console.log("Clicou no veja mais")}
+            />
         </View>
     </ScrollView>
   );
