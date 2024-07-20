a0f7bccd9e9d7232849f04165cb3b4669b948acd
CleitonBrito <cleytonbritto3003@gmail.com>
2024-07-20 20:46:46 -0300

    remove dependeces not used

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
-const flowbite = require("flowbite-react/tailwind")
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
   content: [
     "./src/app/**/*.{js,jsx,ts,tsx}", 
     "./src/components/**/*.{js,jsx,ts,tsx}",
-    flowbite.content()
     ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite.plugin(),
   ],
 }
